5280b0313413d34f62f534beb757e735b4e81ab9
Slikthemighty <slikthemighty@gmail.com>
2022-11-17 22:48:53 -0500

    following tutorial

---
+++ Rounds Testbed/Rounds Testbed/Class1.cs
@@ -0,0 +1,36 @@
+﻿using BepInEx;
+using UnboundLib;
+using UnboundLib.Cards;
+using RoundsTestBed.Cards;
+using HarmonyLib;
+using CardChoiceSpawnUniqueCardPatch.CustomCategories;
+using 
+
+namespace RoundsTestBed
+{
+    // These are the mods required for our mod to work
+    [BepInDependency("com.willis.rounds.unbound", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.moddingutils", BepInDependency.DependencyFlags.HardDependency)]
+    [BepInDependency("pykess.rounds.plugins.cardchoicespawnuniquecardpatch", BepInDependency.DependencyFlags.HardDependency)]
+    // Declares our mod to Bepin
+    [BepInPlugin(ModId, ModName, Version)]
+    // The game our mod is associated with
+    [BepInProcess("Rounds.exe")]
+    public class MyModName : BaseUnityPlugin
+    {
+        private const string ModId = "com.fluorinegas.rounds.RoundsTestbed";
+        private const string ModName = "Rounds Testbed";
+        public const string Version = "1.0.0"; // What version are we on (major.minor.patch)?
+        public const string ModInitials = "RTB";
+        void Awake()
+        {
+            // Use this to call any harmony patch files your mod may have
+            var harmony = new Harmony(ModId);
+            harmony.PatchAll();
+        }
+        void Start()
+        {
+            CustomCard.BuildCard<MyCardName>();
+        }
+    }
+}
